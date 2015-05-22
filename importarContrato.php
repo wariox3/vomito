03d172657e19c338fb5a1be243178f5447be70bf
desarrollo <desarrollo@desarrollo>
2015-05-22 12:57:47 -0500

    Puesta a punto

--- importarContrato.php
+++ importarContrato.php
@@ -6,57 +6,9 @@
     </head>
     <body>
         <h1>Importar contrato</h1>
-        <?php
-        set_time_limit(0);
-        include("conexion.php");
+        <br /><br />
+        <a href="index.php">Volver</a> <a href="importarContratoProceso.php">Importar</a>
         
-        $strSql = "SELECT sql_migracion_contratos.* FROM sql_migracion_contratos WHERE exportado_migracion = 0";
-        $arContatos = $servidorJG->query($strSql);
-
-        if ($arContatos->num_rows > 0) {
-            while($arContrato = $arContatos->fetch_assoc()) {
-                /*$arEmpleados = $servidorBrasa->query("SELECT codigo_empleado_pk FROM rhu_empleado WHERE numero_identificacion = '" . $arContrato['cedemple'] . "'");
-                $arEmpleado = $arEmpleados->fetch_assoc();
-                $codigoEmpleado = $arEmpleado['codigo_empleado_pk'];
-                
-                $strInsertar = "INSERT INTO rhu_contrato "
-                    . " (codigo_empleado_fk, fecha, fecha_desde, fecha_hasta, numero, vr_salario, estado_activo, comentarios, codigo_tipo_tiempo_fk) "                        
-                    . "  VALUES (" . $codigoEmpleado . ", '2015-05-08', '" . $arContrato['fechainic'] . "', '" . $arContrato['fechater'] . "', '" . $arContrato['contrato'] . "', " . $arContrato['salario'] . ", " . $arContrato['estado_activo'] . ", '" . $arContrato['nota'] . "', " . $arContrato['codigo_tipo_tiempo_fk'] . ")";
-                if ($servidorBrasa->query($strInsertar) === TRUE) {
-                    $strActMigracion = "UPDATE contrato SET exportado_migracion = 1 WHERE contrato.contrato = '" . $arContrato['contrato'] . "'";
-                    $servidorJG->query($strActMigracion);
-                } else {
-                    echo "Error: " . $arContrato['contrato'] . ":" .$servidorBrasa->error . "<br />";
-                }*/            
-            }
-            //Actualizar los estados
-            /*$strActualizar = "update rhu_contrato set estado_activo = 0  where fecha_hasta < NOW() AND fecha_hasta <> '0000-00-00'";
-            $strActualizar2 = "update rhu_contrato set fecha_hasta = fecha_desde, indefinido = 1  where fecha_hasta = '0000-00-00'";
-            if ($servidorBrasa->query($strActualizar) === TRUE && $servidorBrasa->query($strActualizar2) === TRUE) {
-                echo "Se creacion y actualizaron correctamente los contratos <br />";
-                $strActualizar = "UPDATE  rhu_empleado 
-                                    LEFT JOIN rhu_contrato ON rhu_empleado.codigo_empleado_pk = rhu_contrato.codigo_empleado_fk 
-                                    SET rhu_empleado.estado_activo = 1, rhu_empleado.contrato_indefinido = 1, rhu_empleado.fecha_contrato = rhu_contrato.fecha_desde,
-                                    rhu_empleado.fecha_finaliza_contrato = rhu_contrato.fecha_hasta, rhu_empleado.vr_salario = rhu_contrato.vr_salario, rhu_empleado.codigo_tipo_tiempo_fk = rhu_contrato.codigo_tipo_tiempo_fk   
-                                    WHERE rhu_contrato.estado_activo = 1";
-                if ($servidorBrasa->query($strActualizar) === TRUE) {
-                    echo "Se creacion y actualizaron correctamente los empleados <br />";
-                } else {
-                    echo "Error en la actualizacion de empleados:" .$servidorBrasa->error . "<br />";
-                }                 
-            } else {
-                echo "Error en la actualizacion de contratos:" .$servidorBrasa->error . "<br />";
-            }*/            
-        } else {
-            echo "0 results";
-        }
-        $arContatos->close();
-        set_time_limit(60);
-        ?>
-                <br /><br />
-                <a href="index.php">Volver</a> <a href="importarContratoProceso.php">Importar</a>
-        
-
 <table border="1">
     <caption>Contratos nuevos</caption>
     <thead>
@@ -71,7 +23,9 @@
     </thead>
     <tbody>                
         <?php
-        $strSql = "SELECT sql_migracion_contratos.* FROM sql_migracion_contratos WHERE exportado_migracion = 0";
+        set_time_limit(0);
+        include("conexion.php");
+        $strSql = "SELECT sql_migracion_contratos.* FROM sql_migracion_contratos WHERE codigo_interface_migracion is not null";
         $arContatos = $servidorJG->query($strSql);        
         if ($arContatos->num_rows > 0) {
             while ($arContato = $arContatos->fetch_assoc()) {
--- importarContratoProceso.php
+++ importarContratoProceso.php
@@ -10,45 +10,47 @@
         set_time_limit(0);
         include("conexion.php");               
         
-        $strSql = "SELECT sql_migracion_contratos.* FROM sql_migracion_contratos WHERE exportado_migracion = 0 limit 10";
+        $strSql = "SELECT sql_migracion_contratos.* FROM sql_migracion_contratos WHERE codigo_interface_migracion is not null";
         $arContatos = $servidorJG->query($strSql);
 
         if ($arContatos->num_rows > 0) {
             while($arContrato = $arContatos->fetch_assoc()) {
-                $arEmpleados = $servidorBrasa->query("SELECT codigo_empleado_pk FROM rhu_empleado WHERE numero_identificacion = '" . $arContrato['cedemple'] . "'");
-                $arEmpleado = $arEmpleados->fetch_assoc();                
-                $strInsertar = "INSERT INTO rhu_contrato "
-                    . " (codigo_empleado_fk, fecha, fecha_desde, fecha_hasta, numero, vr_salario, estado_activo, comentarios, codigo_tipo_tiempo_fk, codigo_centro_costo_fk) "                        
-                    . "  VALUES (" . $arEmpleado['codigo_empleado_pk'] . ", '2015-05-08', '" . $arContrato['fechainic'] . "', '" . $arContrato['fechater'] . "', '" . $arContrato['contrato'] . "', " . $arContrato['salario'] . ", " . $arContrato['estado_activo'] . ", '" . $arContrato['nota'] . "', " . $arContrato['codigo_tipo_tiempo_fk'] . ", " . $arContrato['codigo_interface_migracion'] . ")";
-                
-                /*if ($servidorBrasa->query($strInsertar) === TRUE) {
-                    $strActMigracion = "UPDATE contrato SET exportado_migracion = 1 WHERE contrato.contrato = '" . $arContrato['contrato'] . "'";
-                    $servidorJG->query($strActMigracion);
-                } else {
-                    echo "Error: " . $arContrato['contrato'] . ":" .$servidorBrasa->error . "<br />";
-                }*/            
-            }
-            //Actualizar los estados
-            /*$strActualizar = "update rhu_contrato set estado_activo = 0  where fecha_hasta < NOW() AND fecha_hasta <> '0000-00-00'";
-            $strActualizar2 = "update rhu_contrato set fecha_hasta = fecha_desde, indefinido = 1  where fecha_hasta = '0000-00-00'";
-            if ($servidorBrasa->query($strActualizar) === TRUE && $servidorBrasa->query($strActualizar2) === TRUE) {
-                echo "Se creacion y actualizaron correctamente los contratos <br />";
-                $strActualizar = "UPDATE  rhu_empleado 
-                                    LEFT JOIN rhu_contrato ON rhu_empleado.codigo_empleado_pk = rhu_contrato.codigo_empleado_fk 
-                                    SET rhu_empleado.estado_activo = 1, rhu_empleado.contrato_indefinido = 1, rhu_empleado.fecha_contrato = rhu_contrato.fecha_desde,
-                                    rhu_empleado.fecha_finaliza_contrato = rhu_contrato.fecha_hasta, rhu_empleado.vr_salario = rhu_contrato.vr_salario, rhu_empleado.codigo_tipo_tiempo_fk = rhu_contrato.codigo_tipo_tiempo_fk   
-                                    WHERE rhu_contrato.estado_activo = 1";
-                if ($servidorBrasa->query($strActualizar) === TRUE) {
-                    echo "Se creacion y actualizaron correctamente los empleados <br />";
-                } else {
-                    echo "Error en la actualizacion de empleados:" .$servidorBrasa->error . "<br />";
-                }                 
-            } else {
-                echo "Error en la actualizacion de contratos:" .$servidorBrasa->error . "<br />";
-            }*/            
+                if($arContrato['codigo_interface_migracion'] != "") {
+                    $arEmpleados = $servidorBrasa->query("SELECT codigo_empleado_pk FROM rhu_empleado WHERE numero_identificacion = '" . $arContrato['cedemple'] . "'");
+                    $arEmpleado = $arEmpleados->fetch_assoc();                
+                    $strInsertar = "INSERT INTO rhu_contrato "
+                        . " (codigo_empleado_fk, fecha, fecha_desde, fecha_hasta, numero, vr_salario, estado_activo, comentarios, codigo_tipo_tiempo_fk, codigo_centro_costo_fk) "                        
+                        . "  VALUES (" . $arEmpleado['codigo_empleado_pk'] . ", '2015-05-08', '" . $arContrato['fechainic'] . "', '" . $arContrato['fechater'] . "', '" . $arContrato['contrato'] . "', " . $arContrato['salario'] . ", " . $arContrato['estado_activo'] . ", '" . $arContrato['nota'] . "', " . $arContrato['codigo_tipo_tiempo_fk'] . ", " . $arContrato['codigo_interface_migracion'] . ")";
+
+                    if ($servidorBrasa->query($strInsertar) === TRUE) {
+                        $strActMigracion = "UPDATE contrato SET exportado_migracion = 1 WHERE contrato.contrato = '" . $arContrato['contrato'] . "'";
+                        $servidorJG->query($strActMigracion);
+                    } else {
+                        echo "Error: " . $arContrato['contrato'] . ":" . $strInsertar . " Descripcion: " .$servidorBrasa->error . "<br />";
+                    }                       
+                }        
+            }            
         } else {
-            echo "0 results";
+            echo "0 results <br />";
         }
+        //Actualizar las fechas de terminacion
+        $strSql = "SELECT rhu_contrato.* FROM rhu_contrato WHERE indefinido = 1";
+        $arContatos = $servidorBrasa->query($strSql);                    
+        if ($arContatos->num_rows > 0) {
+            while($arContrato = $arContatos->fetch_assoc()) {                
+                $arContratosJG = $servidorJG->query("SELECT fechater FROM contrato WHERE contrato = '" . $arContrato['numero'] . "'");
+                $arContratoJG = $arContratosJG->fetch_assoc();                     
+                if($arContratoJG['fechater'] != '0000-00-00') {                        
+                    $strActContratoBrasa = "UPDATE rhu_contrato SET indefinido = 0, estado_activo = 0, fecha_hasta = '" . $arContratoJG['fechater'] . "' WHERE codigo_contrato_pk = " . $arContrato['codigo_contrato_pk'];
+                    $servidorBrasa->query($strActContratoBrasa);                        
+                }
+            }
+        }
+        //Actualizar los estados
+        $strActualizar = "update rhu_contrato set estado_activo = 0  where fecha_hasta < NOW() AND fecha_hasta <> '0000-00-00'";
+        $strActualizar2 = "update rhu_contrato set fecha_hasta = fecha_desde, indefinido = 1  where fecha_hasta = '0000-00-00'";
+        $servidorBrasa->query($strActualizar);
+        $servidorBrasa->query($strActualizar2);        
         $arContatos->close();
         set_time_limit(60);
         ?>
