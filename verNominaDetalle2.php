cc174311ddd6489a816d487e847fee80c7f96de9
User <User@PC-SOPORTE.JGEFECTIVOS.LOCAL>
2015-05-28 11:49:24 -0500

    Actualiar dias

--- verNominaDetalle2.php
+++ verNominaDetalle2.php
@@ -14,7 +14,10 @@
         <a href="verNomina.php">Volver</a> <br /> <br />        
         <h1>Ver pago nomina <?php echo $_GET['codigo']; ?></h1>
         <?php 
-        include("conexion.php");        
+        include("conexion.php");  
+        $arNominaMaestros = $servidorJG->query("SELECT sql_migracion_pago_nomina.* FROM sql_migracion_pago_nomina WHERE codigo = '" . $_GET['codigo'] . "'");
+        $arNominaMaestro = $arNominaMaestros->fetch_assoc();
+        echo $arNominaMaestro['zona'] . " [" . $arNominaMaestro['desde'] . " " . $arNominaMaestro['hasta'] . "]";
         include("verNominaDetalleEmpleados.php");
         include("verNominaDetallesSinClasificar.php");
         include("verNominaDetallesNovedades.php");
@@ -24,8 +27,8 @@
         echo "#SQL Adicionales<br />" . $strAdicionales;
         echo "<br /><br />";
         echo "#SQL Suplementario<br />" . $strSuplementario;
-        echo "<br /><br />";
-        echo "#SQL tiempo laborado<br />" . $strTiempoLaborado;
+        //echo "<br /><br />";
+        //echo "#SQL tiempo laborado<br />" . $strTiempoLaborado;
         echo "<br /><br />";
         echo "#SQL incapacidades<br />" . $strNovedadesIncapacidades;
         echo "<br /><br />";
--- verNominaDetallesRetiros.php
+++ verNominaDetallesRetiros.php
@@ -2,18 +2,21 @@
     <caption>Retiros</caption>
     <thead>
         <tr>
-            <th>codretiro</th>                   
+            <th>codretiro</th>   
+            <th>Retiro</th>
+            <th>Nombre</th>
         </tr>
     </thead>
     <tbody>                
-        <?php
-        $strSuplementario = "";
-        $strSql = "SELECT retiroprovision.* FROM retiroprovision WHERE fechare >= '2015-05-16' AND fechare >= '2015-05-30'";
+        <?php        
+        $strSql = "SELECT retiroprovision.* FROM retiroprovision WHERE fechare >= '" . $arNominaMaestro['desde'] . "' AND fechare <= '" . $arNominaMaestro['hasta'] . "' AND zona='" . $arNominaMaestro['zona'] . "' Order by fechare asc";
         $arRetiros = $servidorJG->query($strSql);
         if ($arRetiros->num_rows > 0) {
             while ($arRetiro = $arRetiros->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td>" . $arRetiro['codretiro'] . "</td>";
+                echo "<td>" . $arRetiro['fechare'] . "</td>";
+                echo "<td>" . $arRetiro['nombres'] . "</td>";
                 echo "</tr>";
 
             }
